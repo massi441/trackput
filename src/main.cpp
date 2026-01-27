876de10089ffd0b2bf7cfdbf234d7d827be775f3
Massi Djaiz <djaizmassi@gmail.com>
2026-01-27 12:33:11 -0500

    Add asio dependency with example in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+#include "asio.hpp"
+#include "nlohmann/json.hpp"
+
 int main() {
-    std::cout << "Welcome To Trackput" << std::endl;
+    asio::io_context io;
+    nlohmann::json j;
+
+    j[1] = "JSON OBJECT";
+
+    asio::post(io, [j]{
+        std::cout << "Welcome To Trackput From Asio" << std::endl;
+        std::cout << "This is the content of the json object: " << j[1] << std::endl;
+    });
+
+    io.run();
+
     return 0;
 }
