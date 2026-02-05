9c8da9e5111800e2dc1a2dd174b9d1a68a9fffb0
Massi Djaiz <djaizmassi@gmail.com>
2026-02-04 23:51:53 -0500

    Add test code in each project

---
+++ server/src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "asio.hpp"
+#include "nlohmann/json.hpp"
+
+int main() {
+    asio::io_context io;
+    nlohmann::json j;
+
+    j["Message"] = "Hello from the trackput server!";
+
+    asio::post(io, [j] {
+        std::cout << j["Message"] << std::endl; 
+    });
+
+    io.run();
+
+    return 0;
+}
---
+++ tests/main.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+TEST(Example, Addition) {
+    int num = 2;
+
+    EXPECT_EQ(num, 2);
+}
